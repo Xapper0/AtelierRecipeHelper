adc4c3ad724b5de0377e28bf83b398360c09939e
Xapper <none>
2020-07-01 14:27:25 +0100

    Recipe creation

--- scripts/GraphBuilder.py
+++ scripts/GraphBuilder.py
@@ -1,3 +1,85 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('country_data.xml')
-root = tree.getroot()
+from pprint import pprint
+
+NEW_ITEM = "ItemTag"
+MAKE_NUM = "MakeNum"
+HOURS_NEEDED = "Hour"
+MIN_MATS = "NeedNum"
+SYN_CAT = "RecipeCategory"
+MAT = "MatTag"
+EFFECT = "AddEff"
+START_EFFECT = "MassEffect"
+HAS_DATA = "HasData"
+
+class RecipeMeta:
+    def __init__(self):
+        self.itemEnum = None
+        self.makeNum = None
+        self.hoursNeeded = None
+        self.minMats = None
+        self.synCategory = None
+        self.matsUsed = []
+        self.effects = []
+        self.startEffects = []
+
+#Creates a dictionary of Recipe Meta
+def createRecipeODict(metaPath):
+    xmlp = ET.XMLParser(encoding='utf-8')
+    tree = ET.parse(metaPath, parser=xmlp)
+    root = tree.getroot()
+
+    recipeDict = {}
+    currentRecipe = None
+
+    for item in root:
+        if NEW_ITEM in item.attrib: #new recipe meta
+            if HAS_DATA not in item.attrib: #ignoring placeholders
+                continue
+
+            currentRecipe = RecipeMeta()
+            recipeDict[item.attrib.get(NEW_ITEM)] = currentRecipe
+            
+            currentRecipe.itemEnum = item.attrib.get(NEW_ITEM)
+            currentRecipe.makeNum = item.attrib.get(MAKE_NUM)
+            currentRecipe.hoursNeeded = item.attrib.get(HOURS_NEEDED)
+            currentRecipe.minMats = item.attrib.get(MIN_MATS)
+            currentRecipe.synCategory = item.attrib.get(SYN_CAT)
+
+            currentRecipe.matsUsed.append(item.attrib.get(MAT))
+            currentRecipe.effects.append(getEffects(item.attrib))
+            currentRecipe.startEffects.append(item.attrib.get(START_EFFECT))
+
+        else: #addition to the recipe meta
+            if MAT in item.attrib:
+                currentRecipe.matsUsed.append(item.attrib.get(MAT))
+
+            effects = getEffects(item.attrib)
+            if effects:
+                currentRecipe.effects.append(effects)
+            
+            if START_EFFECT in item.attrib:
+                currentRecipe.startEffects.append(item.attrib.get(START_EFFECT))
+
+    return recipeDict
+
+#Gets the effects and appends them into an array
+def getEffects(itemAttrib):
+    effectList = []
+    effectNo = 0
+    while EFFECT + str(effectNo) in itemAttrib:
+        effectList.append(itemAttrib.get(EFFECT + str(effectNo)))
+        effectNo += 1
+    return effectList
+
+def createRecipes(recipeDict, recipePath):
+    xmlp = ET.XMLParser(encoding='utf-8')
+    tree = ET.parse(recipePath, parser=xmlp)
+    root = tree.getroot()
+
+    recipes = []
+
+    for recipe in root
+    
+
+if __name__ == "__main__":
+    createRecipeODict("data/itemrecipedata.xml")
