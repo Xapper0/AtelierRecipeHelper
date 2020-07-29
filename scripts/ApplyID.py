b1ec66044e462d911becc5a92df71f14a3f47834
Xapper <none>
2020-07-29 16:49:32 +0100

    Added comments and removed useless code

--- scripts/ApplyID.py
+++ scripts/ApplyID.py
@@ -1,49 +1,34 @@
 import csv
-import sys
 import xml.etree.ElementTree as ET
-import glob
 import StringIDToLang
-from enum import Enum
-
-STRING_ID = "String_No"
-TRANSLATION = "Text"
-
-class ryzaEnumStarters(Enum):
-    MATERIAL = "ITEM_MAT"
-    MIX = "ITEM_MIX"
-    WEAPON = "ITEM_WEAPON"
-    ARMOUR = "ITEM_ARMOR"
-    ACCESSORY = "ITEM_ACCESSORY"
-    KEY = "ITEM_KEY"
-    CATEGORY = "ITEM_CATEGORY"
-    EFFECT = "ITEM_EFF"
-    TRAIT = "ITEM_POTENTIAL"
-
-def joinXMLToCSV(pathPattern, writePath):
-    with open(writePath, mode="w") as writeFile:
-        writer = csv.writer(writeFile, lineterminator="\n")
-        for path in glob.glob(pathPattern):
-            tree = ET.parse(path)
-            root = tree.getroot()
-            for item in root:
-                row = []
-                row.append((item.find(".").attrib[STRING_ID]).encode("ascii", errors="ignore").decode())
-                row.append((item.find(".").attrib[TRANSLATION]).encode("ascii", errors="ignore").decode())
-                writer.writerow(row)
 
 def applyID(startNo, idJumps, enumsPath):
+    """Adds the string ids to the enums file 
+
+    Args:
+        startNo (int): The id to start with
+        idJumps (dict of str : int): When the id should change for a given enum
+        enumsPath (str): The path to the enums file
+    """
     with open(enumsPath) as enumsFile, open(enumsPath[:-4] + "_id.csv", mode="w") as writeFile:
         reader = csv.reader(enumsFile)
         writer = csv.writer(writeFile, lineterminator="\n")
 
+        stringID = startNo
         for row in reader:
             if row[0] in idJumps:
-                startNo = idJumps[row[0]]
-            row.append(startNo)
+                stringID = idJumps[row[0]]
+            row.append(stringID)
             writer.writerow(row)
-            startNo = startNo + 1
+            stringID = stringID + 1
 
 def testIDs(enumsPath, translationDictionary):
+    """Used to test that the ID is correctly applied by comparing English names
+
+    Args:
+        enumsPath (str): The path to the enums file where the id is also stored
+        translationDictionary (dict of int : str): The dictionary the compare the English name with
+    """
     with open(enumsPath) as enumsFile:
         reader = csv.reader(enumsFile)
         for row in reader:
@@ -64,6 +49,6 @@ if __name__ == "__main__":
         "ITEM_KIND_MATERIAL":7012353
     }
     applyID(6750209, idJumps, "data/ryza_enums.csv")
-    # applyID(6750209, "data/itemcat.csv")
+
     stringIDToLang = StringIDToLang.createStringIDToLang("data/strcombineall.xml")
     testIDs("data/ryza_enums_id.csv", stringIDToLang)
